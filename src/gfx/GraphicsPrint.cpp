305f29f0a4aaf2846dbd15ae550f4bf18f52d956
Gerben <gerbenvaneerten@gmail.com>
2017-08-28 12:36:59 +0200

    Added empty pixel after setting font char

--- src/gfx/GraphicsPrint.cpp
+++ src/gfx/GraphicsPrint.cpp
@@ -66,10 +66,13 @@ namespace g3rb3n
   void GraphicsPrint::setChar(uint8_t page, uint8_t col, uint8_t c)
   {
     if (!font->has(c)) return;
-
+    uint8_t bm[font->height()/8];
+    for (uint8_t i = 0 ; i < font->height()/8 ; ++i)
+      bm[i] = 0x00;
     uint8_t bytes[font->size()];
     font->get(bytes, c);
     setBitmap(bytes, page, col, font->height() / 8, font->width());
+    setBitmap(bm, page, col + font->width(), font->height() / 8, 1);
   }
 
   void GraphicsPrint::drawCharByPixels(uint8_t x, uint8_t y, uint8_t c)
